9a97a3d31762f7115d8fc363eea472785041360d
djwoun <65102751+djwoun@users.noreply.github.com>
2021-07-23 11:13:32 +0900

    /
    /

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
-const mongoose = require('mongoose')
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-var app = express()
+var app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(process.env.MONGODBURI, {
     useNewUrlParser: true,
   })
-  .then(_ => console.log('Successfully Connected to MongoDB'))
-  .catch(err => console.log('Error connecting to MongoDB', err))
+  .then((_) => console.log("Successfully Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB", err));
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-module.exports = app
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,51 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const User = require('../models/User')
-const FaveMeal = require('../models/FaveMeal')
-const router = express.Router()
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const FaveMeal = require("../models/FaveMeal");
+const router = express.Router();
 
 /* 1. GET all users from the database */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
+  User.find()
+    .then((user) => {
+      // res.status(200);
+      res.send(user);
+    })
+    .catch((err) => {
+      //res.status(400);
+      console.log(err);
+    });
   /* your code here */
-})
+});
 
 /* 2. POST a new user to the database. No need to specify faveMeals; let it use the default */
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
+  const newUser = new User({
+    name: "Jun",
+  });
+  blog
+    .save()
+    .then((user) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   /* your code here */
-})
+});
 
 /* 3. PATCH an existing user by adding a new favorite meal to their list of favorites */
-router.patch('/', function(req, res, next) {
+router.patch("/", function (req, res, next) {
+
+
+  
   /* your code here */
-})
+});
 
 /* 4. GET a list of new users that haven't favorited anything yet */
-router.get('/new', function(req, res, next) {
+router.get("/new", function (req, res, next) {
   /* your code here */
-})
+});
 
-module.exports = router
+module.exports = router;
